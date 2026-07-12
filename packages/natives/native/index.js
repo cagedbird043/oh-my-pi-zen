d057049e631bd6373abb994ee2c94b30bbda758a
cagedbird043 <cagedbird043@gmail.com>
2026-07-12 15:02:18 +0800

    chore(zen): bump version to 16.4.6-zen.1

--- packages/natives/native/index.js
+++ packages/natives/native/index.js
@@ -24,7 +24,7 @@ export const Shell = nativeBindings.Shell;
 
 // functions
 export const __ompInstallTokioRuntime = nativeBindings.__ompInstallTokioRuntime;
-export const __piNativesV16_4_6 = nativeBindings.__piNativesV16_4_6;
+export const __piNativesV16_4_6_zen_1 = nativeBindings.__piNativesV16_4_6_zen_1;
 export const astEdit = nativeBindings.astEdit;
 export const astGrep = nativeBindings.astGrep;
 export const astMatch = nativeBindings.astMatch;
